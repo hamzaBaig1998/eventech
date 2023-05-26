f52a08e5382c5f875e351152f30d7c54690a13d9
Hamza Baig <39631430+hamzaBaig1998@users.noreply.github.com>
2023-05-26 03:58:52 -0400

    blinking message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <header className="App-header">
-      <p>Eventech site coming soon...</p>
+      <p className="coming_soon">Eventech site coming soon...</p>
     </header>
   );
 }
